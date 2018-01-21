36fee0456bd1bf1d4384be057915652a6adee7c8
AndroidGX <subut3x@gmail.com>
2018-01-21 20:33:08 +0100

    brings usual build tools
    Signed-off-by: AndroidGX <subut3x@gmail.com>

---
+++ simplegx_anykernel/anykernel.sh
@@ -0,0 +1,52 @@
+# AnyKernel2 ramdisk mod script
+# osm0sis @ xda-developers
+
+# *****************
+# ANYKERNEL SETUP *
+# *****************
+
+# begin properties
+properties() {
+do.devicecheck=1
+do.modules=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=OnePlus 5T
+device.name2=OnePlus5T
+device.name3=oneplus5T
+device.name4=dumpling
+device.name5=
+} # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+## end setup
+
+# *******************
+# ANYKERNEL METHODS *
+# *******************
+
+# import patching functions/variables - see for reference
+. /tmp/anykernel/tools/ak2-core.sh;
+
+# permissions
+chmod -R 750 $ramdisk/*;
+chown -R root:root $ramdisk/*;
+
+## AnyKernel install
+dump_boot;
+
+# begin ramdisk changes
+
+# init.rc
+insert_line init.qcom.rc "init.simplegx.rc" after "import init.qcom.test.rc" "import init.simplegx.rc";
+insert_line default.prop "ro.sys.fw.bg_apps_limit=60" before "ro.secure=1" "ro.sys.fw.bg_apps_limit=60";
+
+# end ramdisk changes
+
+write_boot;
+
+## end install
